685046952a91e141d3f73ac5c80b4e395c8b5b5a
swatikanishu <nishuswatika@gmail.com>
2022-06-23 22:31:09 +0530

    Added:Employee Functionality Added

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,37 +3,44 @@ import java.util.Random;
 
 public class EmployeeWageBuilder {
 
-   final static int FULL_TIME_WORKER = 1;
+    final static int FULL_TIME_WORKER = 1;
     final static int PART_TIME_WORKER = 2;
     final static int Total_Working_Day = 20;
+    final static int EMP_TOTAL_HOURS = 100;
+    final static int EMP_RATE_PER_HOUR = 20;
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Builder");
-        int EMP_RATE_PER_HOUR = 20;
+        int totalworkingdays = 0;
         int empHours = 0;
         int empWage = 0;
+        int Totalemphrs = 0;
+        System.out.println("Welcome to Employee Wage Builder");
 
-        Random random = new Random();
-        int empCheck = random.nextInt(2);
+        while (Totalemphrs <= EMP_TOTAL_HOURS && totalworkingdays <= Total_Working_Day) {
+            totalworkingdays++;
+            Random random = new Random();
 
+            int empCheck = random.nextInt(2);
 
-        switch (empCheck) {
-            case FULL_TIME_WORKER:
-                empHours = 8;
-                System.out.println("Employee is present");
-                break;
-            case PART_TIME_WORKER:
-                empHours = 4;
-                System.out.println("Employee is present");
-                break;
-            default:
-
-                System.out.println("Employee is absent");
+            switch (empCheck) {
+                case FULL_TIME_WORKER:
+                    empHours = 8;
+                    System.out.println("Employee is present");
+                    break;
+                case PART_TIME_WORKER:
+                    empHours = 4;
+                    System.out.println("Employee is present");
+                    break;
+                default:
+                    empHours=0;
+                    System.out.println("Employee is absent");
+            }
+            Totalemphrs = Totalemphrs + empHours;
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            int Total_emp_wage  = empWage + Totalemphrs;
+            Total_emp_wage = Total_emp_wage * Total_Working_Day;
+            System.out.println("The daily wage of Employee is" + empWage);
+            System.out.println("The Monthly Wage of Employee is" + Total_emp_wage);
         }
-
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        int Total_emp_wage = empWage * Total_Working_Day;
-        System.out.println("The daily wage of Employee is" + empWage);
-        System.out.println("The Monthly Wage of Employee is" + Total_emp_wage);
     }
 }
