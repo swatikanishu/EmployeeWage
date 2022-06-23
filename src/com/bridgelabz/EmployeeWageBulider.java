741e38e67106957c22e767cac709db7d9427be95
swatikanishu <nishuswatika@gmail.com>
2022-06-23 22:09:05 +0530

    Added:Employee Functionality Added

--- src/com/bridgelabz/EmployeeWageBulider.java
+++ src/com/bridgelabz/EmployeeWageBulider.java
@@ -1,8 +1,17 @@
 package com.bridgelabz;
-
+import java.util.Random;
 public class EmployeeWageBulider {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Builder");
+        int IS_fULL_tIME = 1;
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == IS_fULL_tIME) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+
+        }
     }
 
 }
