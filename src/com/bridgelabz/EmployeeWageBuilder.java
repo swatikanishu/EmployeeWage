40bd3294e3e4f04d06025c8b75a470580f0f4126
swatikanishu <nishuswatika@gmail.com>
2022-06-23 22:27:55 +0530

    Added:Employee Functionality Added

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -2,25 +2,28 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWageBuilder {
-    final   static  int FULL_TIME_WORKER=1;
-    final static  int PART_TIME_WORKER=2;
+
+   final static int FULL_TIME_WORKER = 1;
+    final static int PART_TIME_WORKER = 2;
+    final static int Total_Working_Day = 20;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Builder");
-        int EMP_RATE_PER_HOUR=20;
-        int empHours=0;
-        int empWage=0;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHours = 0;
+        int empWage = 0;
 
         Random random = new Random();
         int empCheck = random.nextInt(2);
 
 
-        switch(empCheck){
+        switch (empCheck) {
             case FULL_TIME_WORKER:
-                empHours=8;
+                empHours = 8;
                 System.out.println("Employee is present");
                 break;
             case PART_TIME_WORKER:
-                empHours=4;
+                empHours = 4;
                 System.out.println("Employee is present");
                 break;
             default:
@@ -28,8 +31,9 @@ public class EmployeeWageBuilder {
                 System.out.println("Employee is absent");
         }
 
-        empWage=empHours*EMP_RATE_PER_HOUR;
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        int Total_emp_wage = empWage * Total_Working_Day;
         System.out.println("The daily wage of Employee is" + empWage);
+        System.out.println("The Monthly Wage of Employee is" + Total_emp_wage);
     }
 }
-
