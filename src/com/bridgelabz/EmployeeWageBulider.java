e4f902b52cd9e99c17ff52e949765c647076b390
swatikanishu <nishuswatika@gmail.com>
2022-06-23 22:14:00 +0530

    Added:Employee Functionality Added

--- src/com/bridgelabz/EmployeeWageBulider.java
+++ src/com/bridgelabz/EmployeeWageBulider.java
@@ -4,14 +4,21 @@ public class EmployeeWageBulider {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Builder");
         int IS_fULL_tIME = 1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHours=0;
+        int empWage=0;
         Random random = new Random();
         int empCheck = random.nextInt(2);
+
         if (empCheck == IS_fULL_tIME) {
             System.out.println("Employee is present");
+            empHours=8;
         } else {
             System.out.println("Employee is absent");
-
+            empWage=empHours*EMP_RATE_PER_HOUR;
+            System.out.println("The daliy wage of Empolyee is" + empWage);
         }
     }
-
 }
+
+
