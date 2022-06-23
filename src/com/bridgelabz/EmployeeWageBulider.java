1ee233641137dae6d6f1e661ee9808026874eb09
swatikanishu <nishuswatika@gmail.com>
2022-06-23 22:06:41 +0530

    Added:Welcome message Added

---
+++ src/com/bridgelabz/EmployeeWageBulider.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class EmployeeWageBulider {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Builder");
+    }
+
+}
