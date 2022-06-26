8f99ae03c90aae89fd49a68f5c8010d3c4007a70
swatikanishu <nishuswatika@gmail.com>
2022-06-26 22:47:19 +0530

    Added: Feature Modified

---
+++ src/com/bridgelabz/CompanyWage.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+interface IEmpWageBuilder {
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
+    public void companyWage();
+    public  void printtotalwage();
+    public static int getTotalWage(String companyName){
+        return 0;
+    }
+}
+
+public class CompanyWage {
+    // declaring instance variables
+    final String COMPANY_NAME;
+    final int WAGE_PER_HR;
+    final int MAX_WORKING_DAYS;
+    final int MAX_WORKING_HRS;
+    // instance variable
+    int totalEmpWage;
+
+    public  CompanyWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+        COMPANY_NAME = companyName;
+        WAGE_PER_HR = wagePerHr;
+        MAX_WORKING_DAYS = maxWorkingDays;
+        MAX_WORKING_HRS = maxWorkingHrs;
+        totalEmpWage = 0;
+    }
+    void setTotalEmployeeWage(int totalEmpWage) {
+        this.totalEmpWage = totalEmpWage;
+    }
+    public String toString() {
+        System.out.println("                                                             ");
+        System.out.println("* Total Information of " + COMPANY_NAME + " employee");
+        System.out.println("Wage per hour:" + WAGE_PER_HR);
+        System.out.println("Maximum working days:" + MAX_WORKING_DAYS);
+        System.out.println("Maximum working hours:" + MAX_WORKING_HRS);
+        return "Total wage in a month for " + COMPANY_NAME + " Company is " + totalEmpWage + "\n";
+    }
+}
+
